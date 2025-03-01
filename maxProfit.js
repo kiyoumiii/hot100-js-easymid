e6802ca8040e9d9aba7b4257295820d08c2646e6
kiyoumiii <1749380324@qq.com>
2025-03-01 23:15:26 +0800

    codetop

--- maxProfit.js
+++ maxProfit.js
@@ -1,12 +1,25 @@
 // 对每天的价格，从它的右边一位开始遍历作差，遍历完毕之后的最大值就是最大利润了。
+// 两轮for循环会导致超时
+// var maxProfit = function (prices) {
+//     let res = 0;
+//     for (let i = 0; i < prices.length; i++) {
+//         for(let j = i+1; j < prices.length; j++) {
+//             res = Math.max(res, prices[j] - prices[i]);
+//         }
+//     }
+//     return res;
+// }
 
-var maxProfit = function (prices) {
+// 可以只遍历一遍，边遍历边维护一个在此之前最低的价格minPrice作为买入的价格，实时更新
+var maxProfit = function(prices) {
     let res = 0;
-    for (let i = 0; i < prices.length; i++) {
-        for(let j = i+1; j < prices.length; j++) {
-            res = Math.max(res, prices[j] - prices[i]);
-        }
+    let minPrice = prices[0];
+
+    for(const p of prices) {
+        res = Math.max(res,p-minPrice);
+        minPrice = Math.min(minPrice,p);
     }
+
     return res;
 }
 
