a8a826932a6f0e9630496d56bd82b2efdb604201
kiyoumiii <1749380324@qq.com>
2025-03-15 21:26:25 +0800

    2nd

--- 旋转图像rotate.js
+++ 旋转图像rotate.js
@@ -16,3 +16,23 @@ var rotate = function(matrix) {
         }
     }
 }
+
+// 二刷
+
+var rotate = function(matrix) {
+    let n = matrix.length;
+
+    for (let i = 0; i < Math.floor(n/2); i++) {
+        for (let j = 0; j < n; j++) {
+            [matrix[i][j],matrix[n-i-1][j]] = [matrix[n-i-1][j],matrix[i][j]];
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            [matrix[i][j],matrix[j][i]] = [matrix[j][i],matrix[i][j]];
+        }
+    }
+
+    return matrix;
+};
--- 螺旋矩阵spiralOrder.js
+++ 螺旋矩阵spiralOrder.js
@@ -41,3 +41,53 @@ const matrix = [[1,2,3],[4,5,6],[7,8,9]];
 let res = spiralOrder(matrix);
 console.log(res);
 
+
+// 二刷
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let top = 0, bottom = m-1, left = 0, right = n-1;
+    let res = [];
+
+    while (left <= right && top <= bottom) {
+        for (let i = left; i <= right; i++) {
+            res.push(matrix[top][i]);
+        }
+
+        for (let i = top+1; i <= bottom; i++) {
+            res.push(matrix[i][right]);
+        }
+
+        if(top < bottom && left < right) {
+            for (let i = right-1; i >= left; i--) {
+                res.push(matrix[bottom][i]);
+            }
+            
+            for (let i = bottom-1; i >= top+1; i--) {
+                res.push(matrix[i][left]);
+            }
+        }
+
+        // for (let i = right-1; i >= left; i--) {
+        //     res.push(matrix[bottom][i]);
+        // }
+        
+        // for (let i = bottom-1; i >= top+1; i--) {
+        //     res.push(matrix[i][left]);
+        // }
+
+    
+        left++;
+        right--;
+        top++;
+        bottom--;
+    }
+
+    return res;
+};
+
