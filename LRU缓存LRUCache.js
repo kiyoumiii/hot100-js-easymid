f2e554499b573673b889b5a576d0c167cb51048a
kiyoumiii <1749380324@qq.com>
2025-03-16 20:47:43 +0800

    LRUCache

--- LRU缓存LRUCache.js
+++ LRU缓存LRUCache.js
@@ -1,4 +1,5 @@
 // LRU缓存，用Map数据结构实现
+// 由于 Map 会记住插入顺序，第一个元素就是最近最少使用的元素。
 // 不管是push还是get方法，已经存在就先删掉，这样它才会变成最近使用的
 
 var LRUCahce = function(capacity) {
@@ -45,3 +46,34 @@ console.log(cache.get(2));
 cache.put(4,4);
 console.log(cache.get(1));
 
+// 三刷
+
+var LRUCache = function(capacity) {
+    this.map = new Map();
+    this.limit = capacity;
+};
+
+LRUCache.prototype.get = function(key) {
+    if (this.map.has(key)) {
+        let tmp = this.map.get(key);
+        this.map.delete(key);
+        this.map.set(key,tmp);
+        return tmp;
+    }
+    else {
+        return -1;
+    }
+};
+
+LRUCache.prototype.put = function(key, value) {
+    if (this.map.has(key)) {
+        this.map.delete(key);
+        this.map.set(key,value);
+    }
+    else {
+        this.map.set(key,value);
+        if (this.map.size > this.limit) {
+            this.map.delete((this.map.keys().next().value));
+        }  
+    }
+};
