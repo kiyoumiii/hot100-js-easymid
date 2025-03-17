48d205698572a10a1d817da5e6dd9ecc787fb84a
kiyoumiii <1749380324@qq.com>
2025-03-17 15:36:42 +0800

    tree

--- 对称二叉树.js
+++ 对称二叉树.js
@@ -1,28 +1,32 @@
 // 逻辑是如果是空🌲，或者是没有子节点的单个节点都是对称的return tue;
 // 如果只有一个子树，那么是不对称的return false
-// 如果有左右两个子树，那么就得递归了，让left和right分别递归后left&&right是true那么才是true
+// 如果左子树和右子树的val不相等，则不对称，return false
+// 否则，else，就得递归了，让left和right分别递归比较left.left和right.right，left.right和right.left，之后left_flag&&right_flag是true那么才是true
 
 var isSymmetric = function(root) {
     if (!root) {
         return true;
     }
 
-    function dfs(root) {
-        if (!root.left && !root.right) {
+    function isMirror(left,right) {
+        if (!left && !right) {
             return true;
         }
-        else if (!root.left && root.right) {
+        else if (!left && right) {
             return false;
         }
-        else if (root.left && !root.right) {
+        else if (!right && left) {
             return false;
         }
-        else if (root.left && root.right) {
-            let left = dfs(root.left);
-            let right = dfs(root.right);
-            return (left&&right);
+        else if (left.val != right.val) {
+            return false;
+        }
+        else {
+            let left_flag = isMirror(left.left,right.right);
+            let right_flag = isMirror(left.right,right.left);
+            return (left_flag && right_flag);
         }
     }
 
-    return dfs(root);
-}
+    return isMirror(root.left,root.right);   
+};
